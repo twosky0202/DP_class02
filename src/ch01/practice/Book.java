a5925b102d1f1675e460103b8278c9c71908946e
twosky0202 <leehaneul0202@naver.com>
2023-03-09 10:16:42 +0900

    3월9일수업내용

---
+++ src/ch01/practice/Book.java
@@ -0,0 +1,16 @@
+package ch01.practice;
+
+public class Book {
+    // 속성
+    private String name = "";
+
+    public Book(String name) {
+        this.name = name;
+    }
+    
+    // 메소드
+    public String getName() {
+        return name;
+    } 
+    
+}
