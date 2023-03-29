69f149d8ff618961579ca706e3c69f6b8a3745e5
twosky0202 <leehaneul0202@naver.com>
2023-03-30 04:24:52 +0900

    ch03 TemplateMethod 과제 수정

--- src/hw/ch03/LeeHaNeulDisplay.java
+++ src/hw/ch03/LeeHaNeulDisplay.java
@@ -14,19 +14,19 @@ public class LeeHaNeulDisplay extends AbstractDisplay {
 
     @Override
     public void open() {
-        System.out.println("========================================");
+        System.out.println("================================");
         System.out.println("덕성여대 컴퓨터공학전공");
     }
 
     @Override
     public void print() {
-        System.out.println("이름 : " + name + "/ 학번 : " + studentId + "/ " + grade + "학년");
+        System.out.println("학번 " + studentId + " / " + grade + "학년 / " + name);
     }
 
     @Override
     public void close() {
         // 종료 문자열 ">>"를 표시한다 
         System.out.println("템플릿 메소드 패턴 숙제입니다.");
-        System.out.println("========================================");
+        System.out.println("================================");
     }
 }
