d9da3dc36fa390f0a1cb0f053f3addc13b9b1089
twosky0202 <leehaneul0202@naver.com>
2023-03-30 04:44:51 +0900

    ch02 Adapter 과제 수정

--- src/hw/ch02/FileProperties.java
+++ src/hw/ch02/FileProperties.java
@@ -7,24 +7,29 @@ import java.util.Properties;
 
 // 어댑터 (상속)
 public class FileProperties extends Properties implements FileIO {
+    // Properties property = new Properties();
 
     @Override
     public void readFromFile(String filename) throws IOException {
+        // property.load(new FileReader(filename));
         load(new FileReader(filename));
     }
 
     @Override
     public void writeToFile(String filename) throws IOException {
+        // property.store(new FileWriter(filename), "written by FileProperties");
         store(new FileWriter(filename), "written by FileProperties");
     }
 
     @Override
     public void setValue(String key, String value) {
+        // property.setProperty(key, value);
         setProperty(key, value);
     }
 
     @Override
     public String getValue(String key) {
+        // return property.getProperty(key, "");
         return getProperty(key, "");
     }
 }
--- src/hw/ch02/Main.java
+++ src/hw/ch02/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("20210821 이하늘");
         
-        FileIO f = new FileProperties(); // 어댑터 생성
+        FileIO f = new FileProperties();
 
         try {
             // f.readFromFile("file.txt");
