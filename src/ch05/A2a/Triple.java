d6796631caabffe2c20bc67cf4b2ac0810d18c67
twosky0202 <leehaneul0202@naver.com>
2023-04-04 02:00:15 +0900

    ch05 Singleton

--- src/ch05/A2a/Triple.java
+++ src/ch05/A2a/Triple.java
@@ -6,8 +6,11 @@ import java.util.Map;
 
 public class Triple {
     private static Map<String,Triple> map = new HashMap<>();
-    static {
+                    // key타입, value타입
+    // 클래스 로드 시 실행할 코드 => 해쉬맵을 완성(key: 이름, value : Triple 객체)
+    static {       
         String[] names = { "ALPHA", "BETA", "GAMMA" };
+        // 스트림을 생성 => 각 원소에 대해 => 해쉬맵에 넣어라
         Arrays.stream(names).forEach(s -> map.put(s, new Triple(s)));
     }
 
@@ -19,7 +22,7 @@ public class Triple {
     }
 
     public static Triple getInstance(String name) {
-        return map.get(name);
+        return map.get(name); // 해시맵에서 해당 이름의 Triple 객체를 얻어와서 반환함
     }
 
     @Override
