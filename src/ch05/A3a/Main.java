3704e430e220942e0958a5e8f548fd752f6e8367
twosky0202 <leehaneul0202@naver.com>
2023-04-04 11:11:14 +0900

    ch05 Singleton

--- src/ch05/A3a/Main.java
+++ src/ch05/A3a/Main.java
@@ -3,7 +3,9 @@ package ch05.A3a;
 public class Main extends Thread {
     public static void main(String[] args) {
         System.out.println("Start.");
-        new Main("A").start();
+
+        // 스레드 객체 3개를 만들고, 각각 시작시킴
+        new Main("A").start(); // 스레드의 run을 재동으로 실행함
         new Main("B").start();
         new Main("C").start();
         System.out.println("End.");
--- src/ch05/A3a/Singleton.java
+++ src/ch05/A3a/Singleton.java
@@ -5,19 +5,20 @@ public class Singleton {
 
     private Singleton() {
         System.out.println("인스턴스가 생성되었습니다.");
-        slowdown();
+        slowdown(); // 생성 시 시간이 걸리게 하기 위해서
     }
 
-    public static Singleton getInstance() {
+    // public static Singleton getInstance() {
+    public static synchronized Singleton getInstance() {
         if (singleton == null) {
-            singleton = new Singleton();
+            singleton = new Singleton(); // lazy initialiation
         }
         return singleton;
     }
 
     private void slowdown() {
         try {
-            Thread.sleep(1000);
+            Thread.sleep(1000); // 1초간 쉰다
         } catch (InterruptedException e) {
         }
     }
