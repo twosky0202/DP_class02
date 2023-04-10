c4e6d0a4605f22af743884b1968cc4cb76131b04
twosky0202 <leehaneul0202@naver.com>
2023-04-11 06:28:42 +0900

    :pencil2: ch05 Singleton

--- src/ch05/A3a/Main.java
+++ src/ch05/A3a/Main.java
@@ -5,16 +5,16 @@ public class Main extends Thread {
         System.out.println("Start.");
 
         // 스레드 객체 3개를 만들고, 각각 시작시킴
-        new Main("A").start(); // 스레드의 run을 재동으로 실행함
+        new Main("A").start(); // 스레드의 run을 자동으로 실행함
         new Main("B").start();
         new Main("C").start();
         System.out.println("End.");
     }
 
     @Override
-    public void run() {
+    public void run() { // 이 스레드가 하는 일이 작성되어 있다.
         Singleton obj = Singleton.getInstance();
-        System.out.println(getName() + ": obj = " + obj);
+        System.out.println(getName() + ": obj = " + obj); // obj : 주소
     }
 
     public Main(String name) {
