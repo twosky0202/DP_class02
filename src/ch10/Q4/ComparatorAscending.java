10c388eb643417abcc35f8a211879683672cb5d8
twosky0202 <leehaneul0202@naver.com>
2023-04-13 09:27:50 +0900

    :pencil2: ch10 Strategy 연습문제 10-4

--- src/ch10/Q4/ComparatorAscending.java
+++ src/ch10/Q4/ComparatorAscending.java
@@ -5,9 +5,11 @@ import java.util.Comparator;
 public class ComparatorAscending implements Comparator {
 
     @Override
-    public int compare(Object o1, Object o2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'compare'");
+    public int compare(Object a, Object b) {
+        // 정렬 주체(List)는 음수가 리턴되면 그대로 두고, 양수가 리턴되면 위치를 서로 바꾼다.
+        return ((String)a).compareTo((String)b); // String 클래스의 compareTo()를 이용해서 크기 비교
+        // (a - b)를 실행
+        // a가 작을 때는 음수가 리턴됨, a가 클때는 양수가 리턴
+        // a가 더 클 때 위치를 서로 바꾼다 => 큰 걸 뒤로 보낸다 => 오름차순
     }
-    
 }
--- src/ch10/Q4/Main.java
+++ src/ch10/Q4/Main.java
@@ -6,6 +6,9 @@ class Main {
     public static void main(String[] args) {
         // 문자열 리스트 생성
         List<String> list = Arrays.asList("D", "B", "C", "E", "A");
+        
+        // list: 전략을 이용하는 컨텍스트
+        // Comparator: 전략 객체
 
         // 사전순으로 작은 순서(오름차순)
         list.sort(new ComparatorAscending());
