82d87fc20f47ae719007727aec3f4b0d5576af11
twosky0202 <leehaneul0202@naver.com>
2023-06-06 22:28:51 +0900

    ch14 ChainOfResponsibility 과제 수정

--- src/hw/ch14/Main.java
+++ src/hw/ch14/Main.java
@@ -2,7 +2,7 @@ package hw.ch14;
 
 public class Main {
     public static void main(String[] args) {
-        Support haneul = new PrimeNumberSupport("이하늘");
+        Support leehaneul = new PrimeNumberSupport("이하늘");
         Support alice = new NoSupport("Alice");
         Support bob = new LimitSupport("Bob", 100);
         Support charlie = new SpecialSupport("Charlie", 429);
@@ -11,11 +11,11 @@ public class Main {
         Support fred = new LimitSupport("Fred", 300);
 
         // 사슬 형성
-        haneul.setNext(alice).setNext(bob).setNext(charlie).setNext(diana).setNext(elmo).setNext(fred);
+        leehaneul.setNext(alice).setNext(bob).setNext(charlie).setNext(diana).setNext(elmo).setNext(fred);
 
         // 다양한 트러블 발생
         for (int i = 1; i <= 100; i += 1) {
-            haneul.support(new Trouble(i)); // 맨 앞사람에게만 해결 요구
+            leehaneul.support(new Trouble(i)); // 맨 앞사람에게만 해결 요구
         }
     }
 }
