1412184b1ea3d8b5255ec2ab5a5396b9200bb602
twosky0202 <leehaneul0202@naver.com>
2023-06-07 20:33:34 +0900

    ch18 Memento 연습 문제 18-3

---
+++ src/ch18/A3/game/Memento.java
@@ -0,0 +1,36 @@
+package ch18.A3.game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 상태 보존
+public class Memento {
+    private int money; // 소지금
+    private List<String> fruits; // 과일
+    private int number;
+
+    // 소지금을 얻는다(narrow interface) -> Caretaker는 이것을 통해서만 조작 가능
+    public int getMoney() {
+        return money;
+    }
+
+    // 생성자(wide interface)
+    Memento(int money) { // 같은 패키지 내에서만 호출 가능
+        this.money = money;
+        this.fruits = new ArrayList<>();
+    }
+
+    // 과일을 추가한다(wide interface)
+    void addFruit(String fruit) {
+        fruits.add(fruit);
+    }
+
+    // 과일을 얻는다(wide interface)
+    List<String> getFruits() {
+        return new ArrayList<>(fruits);
+    }
+
+    int getNumber() {
+        return number;
+    }
+}
