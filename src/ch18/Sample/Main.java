82fbf49164ad16d2eb5ed8ff6b30cc263e2b4a29
twosky0202 <leehaneul0202@naver.com>
2023-06-07 20:29:33 +0900

    ch18 Memento 예제, 연습 문제 18-1, 18-2

--- src/ch18/Sample/Main.java
+++ src/ch18/Sample/Main.java
@@ -1,24 +1,30 @@
 package ch18.Sample;
 
 import ch18.Sample.game.Memento;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import ch18.Sample.game.Gamer;
 
+// Caretaker
 public class Main {
     public static void main(String[] args) {
-        Gamer gamer = new Gamer(100);		// 최초 소지금은 100
+        Gamer gamer = new Gamer(100); // 최초 소지금은 100
         Memento memento = gamer.createMemento(); // 최초 상태를 저장해 둔다
+        List<Memento> history = new ArrayList(); // 여러 시점의 상태 보존 가능
 
-        // 게임 시작 
+        // 게임 시작
         for (int i = 0; i < 100; i++) {
-            System.out.println("==== " + i);		// 횟수 표시 
-            System.out.println("상태:" + gamer);	// 현재 주인공의 상태 표시 
+            System.out.println("==== " + i); // 횟수 표시
+            System.out.println("상태:" + gamer); // 현재 주인공의 상태 표시
 
-            // 게임을 진행한다 
+            // 게임을 진행한다
             gamer.bet();
 
             System.out.println("소지금은 " + gamer.getMoney() + "원이 되었습니다.");
 
-            // Memento 취급 방법 결정 
+            // Memento 취급 방법 결정
             if (gamer.getMoney() > memento.getMoney()) {
                 System.out.println("※많이 늘었으니 현재 상태를 저장하자!");
                 memento = gamer.createMemento();
@@ -27,7 +33,7 @@ public class Main {
                 gamer.restoreMemento(memento);
             }
 
-            // 잠시 대기 
+            // 잠시 대기
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
--- src/ch18/Sample/game/Memento.java
+++ src/ch18/Sample/game/Memento.java
@@ -3,17 +3,18 @@ package ch18.Sample.game;
 import java.util.ArrayList;
 import java.util.List;
 
+// 상태 보존
 public class Memento {
-    private int money;             // 소지금 
-    private List<String> fruits;  // 과일
+    private int money; // 소지금
+    private List<String> fruits; // 과일
 
-    // 소지금을 얻는다(narrow interface)
+    // 소지금을 얻는다(narrow interface) -> Caretaker는 이것을 통해서만 조작 가능
     public int getMoney() {
         return money;
     }
 
     // 생성자(wide interface)
-    Memento(int money) {
+    Memento(int money) { // 같은 패키지 내에서만 호출 가능
         this.money = money;
         this.fruits = new ArrayList<>();
     }
@@ -25,6 +26,6 @@ public class Memento {
 
     // 과일을 얻는다(wide interface)
     List<String> getFruits() {
-         return new ArrayList<>(fruits);
+        return new ArrayList<>(fruits);
     }
 }
